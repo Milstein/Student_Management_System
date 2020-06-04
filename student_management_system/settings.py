afc164d18087c9f978f096da92275ccc8ea42e27
Milstein Munakami <milsonmun@yahoo.com>
2020-06-04 11:22:00 -0500

    Second Commit

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Setting media files
 MEDIA_URL = '/media/'
