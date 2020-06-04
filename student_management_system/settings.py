151560f53ee0b3c57896f60eace9697cac9f24a0
Milstein Munakami <milsonmun@yahoo.com>
2020-06-04 12:52:22 -0500

    debug tru

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*4-)jp6*=kgrbik)mlrb3ya3fb15n&se@86nuf(r!&rro@bti4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['milsteinsms.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'milsteinsms.herokuapp.com']
 
 
 # Application definition
