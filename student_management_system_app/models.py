aa7539c9f399e6b7af4b554d8a546782c5129d18
Milstein Munakami <milsonmun@yahoo.com>
2020-06-06 20:39:55 -0500

    middle ware name changed

--- student_management_system_app/models.py
+++ student_management_system_app/models.py
@@ -6,9 +6,9 @@ from django.dispatch import receiver
 
 class SessionYear(models.Model):
     # id = models.AutoField(primary_key=True)
-    session_start=models.DateField()
-    session_end=models.DateField()
-    objects=models.Manager()
+    session_start = models.DateField()
+    session_end = models.DateField()
+    objects = models.Manager()
 
     def __str__(self):
         return '{} TO {}'.format(self.session_start, self.session_end)
@@ -17,7 +17,7 @@ class SessionYear(models.Model):
 
 class CustomUser(AbstractUser):
     user_type_data=((1,"HOD"),(2,"Staff"),(3,"Student"))
-    user_type=models.CharField(default=1,choices=user_type_data,max_length=10)
+    user_type = models.CharField(default=1,choices=user_type_data,max_length=10)
     # gender = models.CharField(max_length=50, choices=(
     #     ('male', 'Male'),
     #     ('female', 'Female')
@@ -25,56 +25,56 @@ class CustomUser(AbstractUser):
 
 
 class AdminHOD(models.Model):    
-    # id=models.AutoField(primary_key=True)
-    admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now_add=True)
-    objects=models.Manager()
+    # id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser,on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
 
 
 class Staff(models.Model):    
-    # id=models.AutoField(primary_key=True)
-    admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
-    address=models.TextField()
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now_add=True)
-    objects=models.Manager()
+    # id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser,on_delete = models.CASCADE)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
 
 
 class Course(models.Model):    
-    # id=models.AutoField(primary_key=True)
-    course_name=models.CharField(max_length=255)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now_add=True)
-    objects=models.Manager()
+    # id = models.AutoField(primary_key=True)
+    course_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
 
     def __str__(self):
         return self.course_name
 
 
 class Subject(models.Model):
-    # id=models.AutoField(primary_key=True)
-    subject_name=models.CharField(max_length=255)
-    course_id=models.ForeignKey(Course,on_delete=models.CASCADE,default=1)
-    staff_id=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now_add=True)
-    objects=models.Manager()
+    # id = models.AutoField(primary_key=True)
+    subject_name = models.CharField(max_length=255)
+    course_id = models.ForeignKey(Course,on_delete = models.CASCADE,default=1)
+    staff_id = models.ForeignKey(CustomUser,on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
 
     def __str__(self):
         return self.subject_name
 
 
 class Student(models.Model):    
-    # id=models.AutoField(primary_key=True)
-    admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
-    gender=models.CharField(max_length=255)
-    profile_pic=models.FileField() # models.ImageField(upload_to = "images/", blank=False)  or FileField(upload_to='images/')
-    address=models.TextField()
-    course_id=models.ForeignKey(Course,on_delete=models.DO_NOTHING)
-    session_year_id = models.ForeignKey(SessionYear, on_delete=models.CASCADE)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now_add=True)
+    # id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser,on_delete = models.CASCADE)
+    gender = models.CharField(max_length=255)
+    profile_pic = models.FileField(max_length=500) # models.ImageField(upload_to = "images/", blank=False)  or FileField(upload_to='images/')
+    address = models.TextField()
+    course_id = models.ForeignKey(Course,on_delete = models.DO_NOTHING)
+    session_year_id = models.ForeignKey(SessionYear, on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
 
@@ -84,40 +84,40 @@ class StudentBulkUpload(models.Model):
 
 
 class Attendance(models.Model):    
-    # id=models.AutoField(primary_key=True)
-    subject_id=models.ForeignKey(Subject, on_delete=models.DO_NOTHING)
-    session_year_id = models.ForeignKey(SessionYear, on_delete=models.CASCADE)
-    attendance_date=models.DateField()
+    # id = models.AutoField(primary_key=True)
+    subject_id = models.ForeignKey(Subject, on_delete = models.DO_NOTHING)
+    session_year_id = models.ForeignKey(SessionYear, on_delete = models.CASCADE)
+    attendance_date = models.DateField()
     
-    created_at=models.DateTimeField(auto_now_add=True)    
-    updated_at=models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)    
+    updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
 
 class AttendanceReport(models.Model):    
-    # id=models.AutoField(primary_key=True)
-    student_id=models.ForeignKey(Student,on_delete=models.DO_NOTHING)
-    attendance_id=models.ForeignKey(Attendance,on_delete=models.CASCADE)
-    status=models.BooleanField(default=False)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now_add=True)
-    objects=models.Manager()
+    # id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Student,on_delete = models.DO_NOTHING)
+    attendance_id = models.ForeignKey(Attendance,on_delete = models.CASCADE)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
 
 
 class LeaveReportStudent(models.Model):    
-    # id=models.AutoField(primary_key=True)
-    student_id=models.ForeignKey(Student,on_delete=models.CASCADE)
-    leave_date=models.CharField(max_length=255)
-    leave_reason=models.TextField()
-    leave_status=models.IntegerField(default=0)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now_add=True)
-    objects=models.Manager()
+    # id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Student,on_delete = models.CASCADE)
+    leave_date = models.CharField(max_length=255)
+    leave_reason = models.TextField()
+    leave_status = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
 
 
 class LeaveReportStaff(models.Model):    
-    # id=models.AutoField(primary_key=True)
-    staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    # id = models.AutoField(primary_key=True)
+    staff_id = models.ForeignKey(Staff, on_delete = models.CASCADE)
     leave_date = models.CharField(max_length=255)
     leave_reason = models.TextField()
     leave_status = models.IntegerField(default=0)
@@ -127,8 +127,8 @@ class LeaveReportStaff(models.Model):
 
 
 class FeedBackStudent(models.Model):    
-    # id=models.AutoField(primary_key=True)
-    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    # id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Student, on_delete = models.CASCADE)
     feedback_message = models.TextField()
     feedback_reply = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -141,27 +141,27 @@ class FeedBackStudent(models.Model):
 
 
 class FeedBackStaff(models.Model):    
-    # id=models.AutoField(primary_key=True)
-    staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    # id = models.AutoField(primary_key=True)
+    staff_id = models.ForeignKey(Staff, on_delete = models.CASCADE)
     feedback_message = models.TextField()
-    feedback_reply=models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
-    objects = models.Manager()
-
-
-class NotificationStudent(models.Model):    
-    # id=models.AutoField(primary_key=True)
-    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
-    message = models.TextField()
+    feedback_reply = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
 
 class NotificationStaff(models.Model):    
-    # id=models.AutoField(primary_key=True)
-    staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    # id = models.AutoField(primary_key=True)
+    staff_id = models.ForeignKey(Staff, on_delete = models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+
+class NotificationStudent(models.Model):    
+    # id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Student, on_delete = models.CASCADE)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
